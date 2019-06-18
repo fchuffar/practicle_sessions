8cb296f8fb0043b6a803e9882a8e25b188f1cf64
Florent Chuffart <florent.chuffart@gmail.com>
2019-06-18 17:39:53 +0200

    Adding odp files and update rnsseq aligment

---
+++ ciment_infrastructure/config_cluster.sh
@@ -0,0 +1,47 @@
+##### 1. Creating account on perseus
+##### https://perseus.ujf-grenoble.fr/
+
+##### 2. Fine tunning of ssh
+# Generate keys, without passphrase (at your own risks)
+ssh-keygen
+# Set ssh configuration
+# use access-ciment machine to access to ciment clusters
+echo "
+Host *
+   StrictHostKeyChecking no
+   HashKnownHosts no
+Host trinity
+   User fchdemo
+   hostname trinity.ujf-grenoble.fr
+Host rotule
+   User fchdemo
+   hostname rotule.imag.fr
+Host luke dahu
+   User fchdemo
+   ProxyCommand ssh fchdemo@access-ciment.imag.fr -W %h:%p
+" > .ssh/config 
+# Copy keys and config on access-ciment (trinity and rotule)
+scp -r .ssh fchdemo@access-ciment.imag.fr:.
+scp -r .ssh trinity:. 
+scp -r .ssh rotule:.
+
+# Connection on access-ciment to authorize keys and copy config to luke
+ssh fchdemo@access-ciment.imag.fr
+cat .ssh/id_rsa.pub > .ssh/authorized_keys
+scp -r .ssh luke:.
+scp -r .ssh dahu:.
+
+##### 3. Fine tunning of ~/.profile
+# On luke (ssh luke)
+# Add epimed environment to your PATH
+echo 'export PATH="/summer/epistorage/opt/bin:$PATH"' >> ~/.profile
+echo 'export PATH="/summer/epistorage/miniconda3/bin:$PATH"' >> ~/.profile
+# export OAR_JOB_KEY_FILE (useful without passphrase to automate supervisation)
+echo 'export OAR_JOB_KEY_FILE=~/.ssh/id_rsa' >> ~/.profile
+
+##### 4. launch resources
+# Interactive connection to ressources
+oarsub -I --project epimed
+# Launch batch on computing element
+oarsub --project epimed "sleep 3600"
+
--- rnaseq_aligment/01_preprocessing.sh
+++ rnaseq_aligment/01_preprocessing.sh
@@ -1,6 +1,7 @@
-## data description
+# # Data description
 # https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE101086
-#
+
+# ## Design
 # SRR5815193 SRX2993641 GSM2699060 WT_RNAseq_rep1
 # SRR5815194 SRX2993642 GSM2699061 WT_RNAseq_rep2
 # SRR5815195 SRX2993643 GSM2699062 WT_RNAseq_rep3
@@ -8,7 +9,7 @@
 # SRR5815200 SRX2993648 GSM2699067 pRnhA_RNAseq_rep2
 # SRR5815201 SRX2993649 GSM2699068 pRnhA_RNAseq_rep3
 
-## download fastq files
+## Download fastq files from NCBI Geo
 mkdir -p ~/projects/datashare/GSE101086/raw
 cd ~/projects/datashare/GSE101086/raw
 parallel-fastq-dump --threads 4 --tmpdir ./ --gzip --split-files --outdir ./ --sra-id SRR5815193
@@ -20,10 +21,21 @@ parallel-fastq-dump --threads 4 --tmpdir ./ --gzip --split-files --outdir ./ --s
 # SR or PE?
 ls -lha ~/projects/datashare/GSE101086/raw
 
-# QC
+
+# # Pipeline "by hand""
+# ## QC
+# Calculation of quality of the reads  
 fastqc SRR5815193_1.fastq.gz
 
-# index genome
+# ## Index genome
+# Create a new StarIndex directory that will group the different indexes created  
+# Launch of STAR software 
+# Set the number of cores according to the server used
+# Command to generate the genome index  
+# Specify the direction in which the directory is located which will contain the saved indexes 
+# Recovery of the reference genome 
+# Recovery of annotations 
+# Specifies the length of the annotated genomic sequence
 mkdir -p ~/projects/datashare/genomes/Schizosaccharomyces_pombe/Ensembl/ASM294v2/Sequence/StarIndex
 STAR \
   --runThreadN 4 \
@@ -33,8 +45,16 @@ STAR \
   --sjdbGTFfile ~/projects/datashare/genomes/Schizosaccharomyces_pombe/Ensembl/ASM294v2/Annotation/Genes/genes.gtf \
   --sjdbOverhang 100
 
-
-# align reads
+# ## Align reads
+# To give the direction of STAR software 
+# Launch of STAR software 
+# Set the number of cores according to the server used 
+# Recovery of annotations 
+# Decompression of file containing  the sequence to map  
+# Reading of file with sequences to map 
+# To define the name of new file including the alignment 
+# To indicate the exit of no map reads of the alignment file 
+# Files BAM sorted thanks to their coordinates 
 cd ~/projects/datashare/GSE101086/
 STAR \
   --runThreadN 4 \
@@ -45,11 +65,24 @@ STAR \
   --outFileNamePrefix GSM2699060_notrim_star_Schizosaccharomyces_pombe_ASM294v2_ \
   --outReadsUnmapped Fastx \
   --outSAMtype BAM SortedByCoordinate
-samtools index 	_notrim_star_Schizosaccharomyces_pombe_ASM294v2_Aligned.sortedByCoord.out.bam
+samtools index GSM2699060_notrim_star_Schizosaccharomyces_pombe_ASM294v2_Aligned.sortedByCoord.out.bam
+
+# ## Count
+# Count of the reads wich have positive sense for each position range of the gene  
+# Recovery of file with reads alignment 
+# Recovery of file with annotations 
+# Retranscription of the number of reads for each position range 
+htseq-count -t exon -f bam -r pos --stranded=yes -m intersection-strict --nonunique none ~/projects/datashare/GSE101086/GSM2699060_notrim_star_Schizosaccharomyces_pombe_ASM294v2_Aligned.sortedByCoord.out.bam ~/projects/datashare/genomes/Schizosaccharomyces_pombe/Ensembl/ASM294v2/Annotation/Genes/genes.gtf > ~/projects/datashare/GSE101086/GSM2699060_notrim_star_Schizosaccharomyces_pombe_ASM294v2_genes_strandedyes_classiccounts.txt
+
+htseq-count -t exon -f bam -r pos --stranded=no -m intersection-strict --nonunique none ~/projects/datashare/GSE101086/GSM2699060_notrim_star_Schizosaccharomyces_pombe_ASM294v2_Aligned.sortedByCoord.out.bam ~/projects/datashare/genomes/Schizosaccharomyces_pombe/Ensembl/ASM294v2/Annotation/Genes/genes.gtf > ~/projects/datashare/GSE101086/GSM2699060_notrim_star_Schizosaccharomyces_pombe_ASM294v2_genes_strandedno_classiccounts.txt
+
+htseq-count -t exon -f bam -r pos --stranded=reverse -m intersection-strict --nonunique none ~/projects/datashare/GSE101086/GSM2699060_notrim_star_Schizosaccharomyces_pombe_ASM294v2_Aligned.sortedByCoord.out.bam ~/projects/datashare/genomes/Schizosaccharomyces_pombe/Ensembl/ASM294v2/Annotation/Genes/genes.gtf > ~/projects/datashare/GSE101086/GSM2699060_notrim_star_Schizosaccharomyces_pombe_ASM294v2_genes_strandedreverse_classiccounts.txt
+# Stranded or not?
 
 
 
-## qc align count
+# # Pipeline using snakemake
+## Linking sample and raw files
 cd ~/projects/datashare/GSE101086/
 echo raw/SRR5815193_1.fastq.gz > GSM2699060_notrim_fqgz.info
 echo raw/SRR5815194_1.fastq.gz > GSM2699061_notrim_fqgz.info
@@ -58,11 +91,15 @@ echo raw/SRR5815199_1.fastq.gz > GSM2699066_notrim_fqgz.info
 echo raw/SRR5815200_1.fastq.gz > GSM2699067_notrim_fqgz.info
 echo raw/SRR5815201_1.fastq.gz > GSM2699068_notrim_fqgz.info
 
-# put wf on luke and luachn 
+## snakemake on local machine 
+cd ~/projects/practicle_sessions/data/GSE101086/
+snakemake -s wf.py --dag 
+snakemake -s wf.py --cores 2 -p
 
+# Put wf on luke and launch it
 rsync -auvP ~/projects/practicle_sessions/data/GSE101086/ luke:~/projects/practicle_sessions/data/GSE101086/
 snakemake -s ~/projects/practicle_sessions/data/GSE101086/wf.py --cores 8 -pn
-
+snakemake -s ~/projects/practicle_sessions/data/GSE101086/wf.py --cores 49 --cluster "oarsub --project epimed -l nodes=1/core={threads},walltime=6:00:00 " -pn
 
 ## get results
 mkdir -p ~/projects/datashare/GSE101086/raw/
